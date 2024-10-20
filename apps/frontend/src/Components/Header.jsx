644bd581dd6550bdfe0e38b5ee274aa3520d4582
Shashank Umar Vaishy <umarshashank@gmail.com>
2024-10-20 15:17:03 +0530

    Designs on signup page,header and mainContent

--- apps/frontend/src/Components/Header.jsx
+++ apps/frontend/src/Components/Header.jsx
@@ -15,6 +15,32 @@ const NavLinks = styled.div`
   gap: 2rem;
   font-size: 1.2rem;
   font-family: 'Courier New', Courier, monospace;
+
+  //new code 
+  a {
+    color: white;
+    position: relative;
+    text-decoration: none;
+    padding-bottom: 2px;
+    
+    &::before {
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: 2px;
+      bottom: 0;
+      left: 0;
+      background-color: white;
+      visibility: hidden;
+      transform: scaleX(0);
+      transition: all 0.3s ease-in-out;
+    }
+
+    &:hover::before {
+      visibility: visible;
+      transform: scaleX(1);
+    }
+  } 
 `;
 
 const SignUpButton = styled(Link)`
@@ -24,6 +50,25 @@ const SignUpButton = styled(Link)`
   border-radius: 5px;
   font-weight: bold;
   text-decoration: none;
+
+  //added later
+   &:hover {
+    background-color: green;
+    color: white;
+    animation: zoom 1.2s ease infinite;
+    animation:prop 0.2s ease 1;
+   
+  }
+    
+  @keyframes prop{
+    from{
+    background-color:white;
+    }
+    to{
+    background-color:green;
+    color:white;
+    }
+  }
 `;
 
 const Logo = styled.div`
