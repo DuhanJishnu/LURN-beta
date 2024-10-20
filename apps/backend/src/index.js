3e33fb7a055433cf9606e0f62c4a6deeef1ac081
Charity Rymbai <charityrymbai@hotmail.com>
2024-10-20 12:27:37 +0530

    added route folders

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -1,7 +1,5 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { CreateUserSchema } from '../zod/schema'
+import authRouter from './Routes/authRoutes'
 
 const app = new Hono()
 
@@ -9,58 +7,7 @@ app.get('/health', (c) => {
     return c.json({ status: 'ok' })
 });
 
-app.post('/signin', async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
-  const body = await c.req.json();
-
-  const parsed = CreateUserSchema.safeParse(body);
-  if (!parsed.success) {
-      return c.json(
-          {
-              message: 'Wrong inputs',
-          },
-          400
-      );
-  }
-
-  try {
-      const existingUser = await prisma.user.findFirst({
-          where: {
-              email: body.email
-          },
-      });
-
-      if (existingUser) {
-          return c.json(
-              {
-                  message: 'User already exists',
-              },
-              400
-          );
-      }
-      const res = await prisma.user.create({
-          data: body,
-      });
-      return c.json(
-          {
-              message: 'Created User',
-          },
-          200
-      );
-  } catch (error) {
-      console.error(error);
-      return c.json(
-          {
-              message: 'Error creating User',
-          },
-          500
-      );
-  } finally {
-      await prisma.$disconnect();
-  }
-})
+app.route('/api/v1/auth/', authRouter);
 
 async function ai(c) {
     const apiKey = c.env.GEMINI_API_KEY
